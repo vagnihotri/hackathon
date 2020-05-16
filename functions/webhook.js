fa3cce87740460143330815a922b2ad63cbd35d5
Joshua Prayogi Angki <joshua@clevertap.com>
2020-05-16 19:14:31 +0700

    initial code for UserProfile upload

--- functions/webhook.js
+++ functions/webhook.js
@@ -10,8 +10,28 @@ exports.webhookfn = functions.https.onRequest((req, res) => {
     }
     return cors(req, res, () => {
       console.log(req.method);
-      console.log(req.body);
-      console.log(req.query);
+      console.log(req.headers);
+      console.log();
+
+      
+      var request = require('request');
+
+      var headerData = req.headers;
+      var dataString = JSON.stringify(req.body);
+      var options = {
+          url: 'https://api.clevertap.com/1/upload',
+          method: 'POST',
+          headers: req.headers,
+          body: dataString
+      };
+
+      function callback(error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body);
+          }
+      }
+
+      request(options, callback);
       res.send(200);
     });
 });
